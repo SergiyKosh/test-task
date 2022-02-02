711b679e684ffaa7f1bf5b3749282fe976516ed4
SergiyKosh <sergey.mov8@gmail.com>
2022-02-02 04:58:33 +0200

    [test-task] progress bar was implemented

--- app/src/main/java/ua/test/task/activity/WebViewActivity.java
+++ app/src/main/java/ua/test/task/activity/WebViewActivity.java
@@ -1,24 +1,45 @@
 package ua.test.task.activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import ua.test.task.R;
 
 public class WebViewActivity extends AppCompatActivity {
     private WebView webView;
+    private ProgressBar progressBar;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setMax(100);
 
         webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://www.google.com.ua/");
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+
+                if (newProgress == 100) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        webView.loadUrl("https://www.youtube.com/");
     }
 }
